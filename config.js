346f305dbeea87baf88d9118f389a5b4d6728f4a
Julian Moran Irigaray <j.moran+globant@globant.com>
2026-04-05 20:20:42 -0300

    config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production'
 
 export const CONFIG = {
-  MODEL_AI: process.env.MODEL_AI ?? (isProduction ? 'llama-3.3-70b-versatile' : 'llama3.2')
+  MODEL_AI: process.env.MODEL_AI ?? (isProduction ? 'llama-3.3-70b-versatile' : 'llama3.2:latest')
 }
