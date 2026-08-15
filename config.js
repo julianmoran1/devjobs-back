629bc293db28855a891e3b3843eb6f9dce7eaf1a
Julian Moran Irigaray <julianmoran1@gmail.com>
2026-08-15 13:11:38 -0300

    model change

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production'
 
 export const CONFIG = {
-  MODEL_AI: process.env.MODEL_AI ?? (isProduction ? 'llama-3.3-70b-versatile' : 'llama3.2:latest')
+  MODEL_AI: process.env.MODEL_AI ?? (isProduction ? 'openai/gpt-oss-120b' : 'llama3.2:latest')
 }
